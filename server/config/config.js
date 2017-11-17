b6b8db5948fec26e596dd3b8bcdab68bd1292660
sschahil <schahil31@gmail.com>
2017-11-16 19:26:06 -0800

    Changed config to develpment mode Modified index.js to close after starting up

--- server/config/config.js
+++ server/config/config.js
@@ -7,7 +7,7 @@ var config = {
 	port: process.env.PORT || 3000
 };
 
-process.env.NODE_ENV = process.env.NODE_ENV || config.test;
+process.env.NODE_ENV = process.env.NODE_ENV || config.dev;
 config.env = process.env.NODE_ENV;
 
 var envConfig;
